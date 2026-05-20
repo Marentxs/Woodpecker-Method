87dd39a06913e60744446aa8c311497183515b9c
Julian <111262742+Marentxs@users.noreply.github.com>
2026-05-20 00:54:37 -0500

    feath(chess): add visual update for oppositeMove

--- src/index.js
+++ src/index.js
@@ -66,6 +66,7 @@ const config = {
 
     if (opponentMoves[currentMoveIndex]) {
       chess.move(opponentMoves[currentMoveIndex]);
+      board.setPosition(chess.fen());
     }
 
     currentMoveIndex++;
